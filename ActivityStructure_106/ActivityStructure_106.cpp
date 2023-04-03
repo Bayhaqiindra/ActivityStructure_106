567fd5e3fb447edb7cbc66980a457b3d94a9d56c
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-03 10:32:44 +0700

    menampilkan hasil data mahasiswa 1

--- ActivityStructure_106/ActivityStructure_106.cpp
+++ ActivityStructure_106/ActivityStructure_106.cpp
@@ -25,3 +25,15 @@ int main() {
 	cin >> mhs2.alamat;
 	cout << "Masukkan Umur :";
 	cin >> mhs2.umur;
+
+	cout << "\nNIM = " << mhs1.NIM;
+	cout << "\nNama = " << mhs1.nama;
+	cout << "\nAlamat = " << mhs1.alamat;
+	cout << "\nUmur = " << mhs1.umur;
+
+	cout << "\n\nNIM = " << mhs2.NIM;
+	cout << "\nNama = " << mhs2.nama;
+	cout << "\nAlamat = " << mhs2.alamat;
+	cout << "\nUmur = " << mhs2.umur;
+
+}
