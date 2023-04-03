567404796eb2d6ce42f7984f0e685e8352e825fa
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-03 10:31:21 +0700

    memasukkan data mahasiswa 2

--- ActivityStructure_106/ActivityStructure_106.cpp
+++ ActivityStructure_106/ActivityStructure_106.cpp
@@ -16,3 +16,12 @@ int main() {
 	mhs1.nama = "Bayhaqi Indra Maulana";
 	mhs1.alamat = "Riau";
 	mhs1.umur = 18;
+
+	cout << "Masukkan NIM :";
+	cin >> mhs2.NIM;
+	cout << "Masukkan Nama :";
+	cin >> mhs2.nama;
+	cout << "Masukkan Alamat :";
+	cin >> mhs2.alamat;
+	cout << "Masukkan Umur :";
+	cin >> mhs2.umur;
