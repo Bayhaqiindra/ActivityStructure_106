9960acf233ccebbbfb25bf79566f2b73581cb08a
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-03 10:17:01 +0700

    membuat struktur data

--- ActivityStructure_106/ActivityStructure_106.cpp
+++ ActivityStructure_106/ActivityStructure_106.cpp
@@ -8,17 +8,3 @@ struct mahasiswa
 	string alamat;
 	int umur;
 };
-
-int main() {
-	mahasiswa mhs1, mhs2;
-
-	mhs1.NIM = "20220140106";
-	mhs1.nama = "Bayhaqi Indra Maulana";
-	mhs1.alamat = "Riau";
-	mhs1.umur = 18;
-
-	cout << "Masukkan NIM :";
-	cin >> mhs2.NIM;
-	cout << "Masukkan Nama :";
-	cin << mhs2.nama;
-}
